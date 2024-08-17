a4a4d8d1a734632e059ea1b346d870da5eab65fa
Abhishek Wadile <130994831+Abhiwadile@users.noreply.github.com>
2024-08-17 12:12:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+<script>
+        // Optional: Control playback using JavaScript
+        const music = document.getElementById('background-music');
+        music.volume = 0.5; // Set volume (0.0 - 1.0)
+    </script>
